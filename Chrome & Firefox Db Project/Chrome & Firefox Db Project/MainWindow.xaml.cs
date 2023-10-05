2b2c0a135005d9730719f2a5443daf5f2084913f
MaMaDDDMD <M.H.damad@chmail.ir>
2023-10-05 14:36:05 +0330

    Creating Project And Installing Nugets

---
+++ Chrome & Firefox Db Project/Chrome & Firefox Db Project/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Chrome___Firefox_Db_Project
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
