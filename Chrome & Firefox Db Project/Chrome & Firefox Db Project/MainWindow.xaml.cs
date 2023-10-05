a3ada22d5dd0c82fadfadd7c140ecf7efe2ee82c
MaMaDDDMD <M.H.damad@chmail.ir>
2023-10-05 16:11:16 +0330

    Completing ViewModel

--- Chrome & Firefox Db Project/Chrome & Firefox Db Project/MainWindow.xaml.cs
+++ Chrome & Firefox Db Project/Chrome & Firefox Db Project/MainWindow.xaml.cs
@@ -1,12 +1,26 @@
-﻿using System.Windows;
+﻿using Chrome___Firefox_Db_Project.ViewModel;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Chrome___Firefox_Db_Project
 {
     public partial class MainWindow : Window
     {
+        readonly DataViewModel viewmodel = new();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) => DataContext = viewmodel;
+
+        private void QueryText_KeyUp(object sender, KeyEventArgs e)
+        {
+            Processing.Visibility = Visibility.Visible;
+            string Query = QueryText.Text;
+            if (e.Key == Key.Enter && !(viewmodel.ExecuteQuery(Query)))
+                MessageBox.Show("Invalid Query!...");
+            Processing.Visibility = Visibility.Collapsed;
         }
     }
 }
---
+++ Chrome & Firefox Db Project/Chrome & Firefox Db Project/ViewModel/DataViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using System.ComponentModel;
+using System.Dynamic;
+
+namespace Chrome___Firefox_Db_Project.ViewModel
+{
+    public class DataViewModel : INotifyPropertyChanged
+	{
+		dynamic? showingdata = new ExpandoObject();
+        public event PropertyChangedEventHandler? PropertyChanged;
+		public dynamic? ShowingData
+		{
+			get { return showingdata; }
+			set { showingdata = value; RaisePropertyChanged(nameof(ShowingData)); }
+		}
+		public void RaisePropertyChanged(string PropertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+		public bool ExecuteQuery(string Query)
+		{
+            using SqliteConnection MediaHistoryDb = new("Data Source=C:\\sqlite\\db\\Media History.db");
+            using SqliteConnection PermissionsDb = new("Data Source=C:\\sqlite\\db\\permissions.db");
+            using SqliteConnection RecentlyClosedTabsDb = new("Data Source=C:\\sqlite\\db\\recently_closed_tabs.db");
+            bool Flag = true;
+            ShowingData = null;
+            try
+            {
+                ShowingData = MediaHistoryDb.Query<dynamic>(Query);
+            }
+            catch
+            {
+                try
+                {
+                    ShowingData = PermissionsDb.Query<dynamic>(Query);
+                }
+                catch
+                {
+                    try
+                    {
+                        ShowingData = RecentlyClosedTabsDb.Query<dynamic>(Query);
+                    }
+                    catch
+                    {
+                        Flag = false;
+                    }
+                }
+            }
+            return Flag;
+        }
+    }
+}
